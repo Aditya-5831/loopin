c5ccbbb0e70a20ea0ba2a9af5837096aec708e99
Aditya-5831 <adityashiyale7219@gmail.com>
2025-06-05 14:42:23 +0530

    Added Deleting post functionality

---
+++ src/modules/posts/actions.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { validateRequest } from "@/auth";
+import db from "@/lib/prisma";
+import { PostDataInclude } from "@/lib/types";
+import { createPostSchema } from "@/lib/validation";
+
+export const submitPost = async (input: string) => {
+  const { user } = await validateRequest();
+
+  if (!user) {
+    throw new Error("User not authenticated");
+  }
+
+  const { content } = createPostSchema.parse({ content: input });
+
+  const newPost = await db.post.create({
+    data: {
+      content,
+      userId: user.id,
+    },
+    include: PostDataInclude,
+  });
+
+  return newPost;
+};
+
+export const deletePost = async (id: string) => {
+  const { user } = await validateRequest();
+
+  if (!user) {
+    throw Error("Unauthorized");
+  }
+
+  const post = await db.post.findUnique({
+    where: { id },
+  });
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  if (post.userId !== user.id) {
+    throw new Error("Unauthorized");
+  }
+
+  const deletedPost = await db.post.delete({
+    where: { id },
+    include: PostDataInclude,
+  });
+
+  return deletedPost;
+};
---
+++ src/modules/posts/mutations.ts
@@ -0,0 +1,103 @@
+import { PostPage } from "@/lib/types";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { deletePost, submitPost } from "./actions";
+
+export const useSubmitPostMutation = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: submitPost,
+    onSuccess: async (newPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed", "for-you"] };
+
+      await queryClient.cancelQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          const firstPage = oldData?.pages[0];
+
+          if (firstPage) {
+            return {
+              pageParams: oldData.pageParams,
+              pages: [
+                {
+                  posts: [newPost, ...firstPage.posts],
+                  nextCursor: firstPage.nextCursor,
+                },
+                ...oldData.pages.slice(1),
+              ],
+            };
+          }
+        },
+      );
+
+      queryClient.invalidateQueries({
+        queryKey: queryFilter.queryKey,
+        predicate(query) {
+          return !query.state.data;
+        },
+      });
+
+      toast.success("Post created");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error("Failed to post. Please try again.");
+    },
+  });
+
+  return { mutate, isPending };
+};
+
+export const useDeletePostMutation = () => {
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: async (deletedPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed"] };
+
+      await queryClient.cancelQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          if (!oldData) {
+            return;
+          }
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              nextCursor: page.nextCursor,
+              posts: page.posts.filter((p) => p.id !== deletedPost.id),
+            })),
+          };
+        },
+      );
+
+      toast.success("Post deleted");
+
+      if (pathname === `/posts/${deletedPost.id}`) {
+        router.push(`/users/${deletedPost.user.username}`);
+      }
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Failed to delete post. Please try again.");
+    },
+  });
+
+  return mutation;
+};
